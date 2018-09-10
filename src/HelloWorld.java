5a571b1955e481c3d826d0e98310808e73279f84
ylaichenkov <ylaichenkov@gmail.com>
2018-09-10 19:25:54 +0300

    Remove ignored files

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         String myName = "Ivan";
         int myAge = 32;
-        System.out.println("Hi! My name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("Hello World! My name is " + myName + " and I am " + myAge + " years old.");
     }
 }
