b4596647134323e39a8fea38d28089d6ea515325
ylaichenkov <ylaichenkov@gmail.com>
2018-09-10 19:10:09 +0300

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String myName = "Ivan";
+        int myAge = 32;
+        System.out.println("Hi! My name is " + myName + " and I am " + myAge + " years old.");
+    }
+}
