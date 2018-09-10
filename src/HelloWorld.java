cc2ce91dbfcc0a40306e141bd4377ddc1784bd84
ylaichenkov <ylaichenkov@gmail.com>
2018-09-10 19:27:38 +0300

    finished lesson #1.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String myName = "Ivan";
-        int myAge = 32;
-        System.out.println("Hello World! My name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("Hello World!");
     }
 }
